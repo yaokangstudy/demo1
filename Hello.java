d70d299536625f6b50fdde924c0b95e6fe431cd4
yaokangstudy <1549584522@qq.com>
2020-05-10 16:48:14 +0800

    hello

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("333");
+    }
+}
